30aa6eb93b56fddf878cb51d98ed40e6f773b43a
anil kumar <anil.pccs7@gmail.com>
2020-11-20 12:27:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'custompreloadstrategy';
+export class AppComponent implements OnInit {
+  title = 'advanced-preloading';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.activatedRoute.children) {
+          const childRoutes = this.activatedRoute.children;
+          childRoutes.forEach((childRoute) => {
+            childRoute.routeConfig.children?.forEach((route) => {
+              if (!route.canActivate && route.loadChildren && route.data?.preload) {
+                route[this.loadChildren]?.();
+              }
+            });
+          });
+        }
+      }
+    });
+  }
+  private get loadChildren(): string {
+    return 'loadChildren';
+  }
 }
