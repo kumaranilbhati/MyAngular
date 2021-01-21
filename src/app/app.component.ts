b3487e5d3514a7f2c03a6ae2d8f8aedeb4809b13
anil kumar <anil.pccs7@gmail.com>
2021-01-21 21:20:45 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,30 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'advanced-preloading';
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private router: Router
-  ){}
+  // constructor(
+  //   private activatedRoute: ActivatedRoute,
+  //   private router: Router
+  // ){}
 
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        if (this.activatedRoute.children) {
-          const childRoutes = this.activatedRoute.children;
-          childRoutes.forEach((childRoute) => {
-            childRoute.routeConfig.children?.forEach((route) => {
-              if (!route.canActivate && route.loadChildren && route.data?.preload) {
-                route[this.loadChildren]?.();
-              }
-            });
-          });
-        }
-      }
-    });
-  }
-  private get loadChildren(): string {
-    return 'loadChildren';
-  }
+  // ngOnInit(): void {
+  //   this.router.events.subscribe((event) => {
+  //     if (event instanceof NavigationEnd) {
+  //       if (this.activatedRoute.children) {
+  //         const childRoutes = this.activatedRoute.children;
+  //         childRoutes.forEach((childRoute) => {
+  //           childRoute.routeConfig.children?.forEach((route) => {
+  //             if (!route.canActivate && route.loadChildren && route.data?.preload) {
+  //               route[this.loadChildren]?.();
+  //             }
+  //           });
+  //         });
+  //       }
+  //     }
+  //   });
+  // }
+  // private get loadChildren(): string {
+  //   return 'loadChildren';
+  // }
 }
